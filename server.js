1a7fbada566eaeed923b72de472164e10ad7d159
largavidaalcodigo <tomas.83.gonzalez@gmail.com>
2018-07-29 09:41:06 -0400

    pasando a prod.. .vamos chile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('errorhandler');
 const session = require('express-session');
-const passport = require('passport');
+//const passport = require('passport');
 const routes = require('./server/routes');
 const flash = require('connect-flash');
 
@@ -25,16 +25,17 @@ app.use(bodyParser.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(errorHandler());
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 app.use(flash());
 
 // Passport configuration
-require('./server/config/auth');
+// require('./server/config/auth');
 
 
 
 // Angular DIST output folder
+/*
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', routes.site.loginForm);
@@ -48,7 +49,7 @@ app.post('/dialog/authorize/decision', routes.oauth2.decision);
 app.post('/oauth/token', routes.oauth2.token);
 
 app.get('/api/userinfo', routes.user.info);
-app.get('/api/clientinfo', routes.client.info);
+app.get('/api/clientinfo', routes.client.info); */
 
 
 // API location
